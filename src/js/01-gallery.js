0560f546ea8641df2845f1e5b706cfc8ec2d4430
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-25 14:03:13 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,8 +4,6 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
-
 const listImg = document.querySelector('.gallery');
 
 const createItems = galleryItems
