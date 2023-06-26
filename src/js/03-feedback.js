914677c2e8db096c06cb0f4636c05d8a8a27157d
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-26 18:32:58 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+emailInput.addEventListener('input', event => {
+  console.log(event.value);
+});
