a78b6d44d2b5dd13df434553ff5fe434c72de70b
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-26 16:08:13 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 'use strict';
+
+import Vimeo from '@vimeo/player';
+
+const video = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(video);
+
+player.on('play', function () {
+  console.log('video jest odtwarzane');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
