d99cb2dac8b099fe194fb33bd2d6c3ddd441f71b
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-27 12:44:48 +0200

    update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,43 @@
 'use strict';
 
+import throttle from 'lodash.throttle';
+
 const emailInput = document.querySelector('input[name="email"]');
 const messageInput = document.querySelector('textarea[name="message"]');
 
-emailInput.addEventListener('input', event => {
-  console.log(event.value);
+function saveStateForm() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(fromState));
+}
+
+function loadStateForm() {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  } else {
+    emailInput.value = '';
+    messageInput.value = '';
+  }
+}
+
+form.addEventListener('input', _.throttle(saveFormState, 500));
+window.addEventListener('load', loadStateForm);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+
+  const formState = {
+    email: '',
+    message: '',
+  };
+  console.log('wyslano formularz:', formState);
 });
